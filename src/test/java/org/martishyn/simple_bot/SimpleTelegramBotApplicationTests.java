7f030d87d2fd7f6434835226479574d99dd3ed45
Andrii Martishyn <Andrii.martishyn@techwave.net>
2023-06-03 15:25:50 +0300

    Initial commit

---
+++ src/test/java/org/martishyn/simple_bot/SimpleTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.martishyn.simple_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
